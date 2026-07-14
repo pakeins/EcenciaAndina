deaa063c70e54eba668f3d67fd8804cb2a10b4a7
Packeins <esteban9696e.c@gmail.com>
2026-07-14 16:44:41 -0500

    chore: use v8 coverage with singleThread to fix dropping coverage across files

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    pool: 'forks', // Usa procesos hijos en lugar de hilos (worker_threads) para evitar memory leaks o crashes con supertest
-    fileParallelism: false, // EJECUTA LOS TESTS EN SERIE PARA EVITAR EL BUG DE COVERAGE DE V8
+    pool: 'threads',
+    poolOptions: {
+      threads: {
+        singleThread: true
+      }
+    },
     setupFiles: ['dotenv/config'],
     coverage: {
-      provider: 'istanbul',
+      provider: 'v8',
       all: true,
       reporter: ['text', 'lcov', 'html'],
       reportsDirectory: './coverage',
